d7343b09c6436c1fe1175f54a695a502ca7f118d
bonsa <bonsa>
2024-06-30 23:08:27 +0300

    second

--- shopping/src/main.jsx
+++ shopping/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Home from './page_routes/home.jsx'
 import {
   createBrowserRouter,
   RouterProvider,
@@ -8,7 +9,7 @@ import {
 const router =createBrowserRouter([
   {
     path:"/",
-    element:<div>diffault page</div>
+    element:<Home/>
   },
 ]
 )
---
+++ shopping/src/page_routes/home.jsx
@@ -0,0 +1,10 @@
+export default function Home(){
+
+    return <div>
+        <h1>THE TITLE</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Saepe iure eos rerum, nobis laboriosam necessitatibus perspiciatis error, 
+            est esse dolorem ducimus quasi odit! Consequatur nulla iste placeat maxime nisi ex!</p>
+            <footer>at last some footer</footer>
+    </div>
+}
